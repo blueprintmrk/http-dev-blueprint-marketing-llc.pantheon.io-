d52acb9e628acb712efe40d965d72d8cb9e31b61
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-03-15 19:24:22 -0700

    Add docker-outer and docker-inner scripts.

---
+++ scripts/docker-inner.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#
+#
+set -ex
+bin="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+if [ "$#" -ne 4 ]; then
+  echo "specify a channel, rpm dir, build num, and revision"
+  exit 1
+fi
+
+channel=$1
+rpm_dir=$2
+build=$3
+epoch=$4
+
+if [ -n "$(git status --porcelain)" ]
+    then
+    echo >&2
+    echo "Error: uncommitted changes present. Please commit to continue." >&2
+    echo "Git commithash is included in rpm, so working tree must be clean to build." >&2
+    exit 1
+fi
+
+fedora_release=$(rpm -q --queryformat '%{VERSION}\n' fedora-release)
+GITSHA=$(git log -1 --format="%h")
+name="wp-cli-0-$channel"
+
+version=$(cat $bin/../VERSION.txt)
+iteration=${epoch}.${GITSHA}
+arch='x86_64'
+url="https://github.com/pantheon-systems/${name}"
+vendor='Pantheon'
+description='wp-cli: Pantheon rpm containing commandline tool for WordPress'
+install_prefix="/opt/pantheon/$name"
+
+download_dir="$bin/../build"
+
+rm -rf $download_dir
+mkdir -p $download_dir
+wget https://github.com/wp-cli/wp-cli/releases/download/v${version}/wp-cli-${version}.phar --output-document=$download_dir/wp-cli-${version}.phar
+
+
+fpm -s dir -t rpm  \
+    --name "${name}" \
+    --version "${version}" \
+    --iteration "${iteration}" \
+    --architecture "${arch}" \
+    --url "${url}" \
+    --vendor "${vendor}" \
+    --description "${description}" \
+    --prefix "$install_prefix" \
+    -C build \
+    wp-cli.phar
+
+
+if [ ! -d "$rpm_dir/$fedora_release/wp-cli" ]  ; then
+  mkdir -p $rpm_dir/$fedora_release/wp-cli
+fi
+
+mv *.rpm $rpm_dir/$fedora_release/wp-cli/
---
+++ scripts/docker-outer.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+set -e
+bin="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+docker=$(which docker)
+
+# Which fedora distros to build this rpm for
+# wp-cli only runs on App servers; we do not have any
+# app servers older than f22.
+BUILD_VERSIONS=${BUILD_VERSIONS:-22}
+
+echo "==> Running RPM builds for these Fedora version(s): $BUILD_VERSIONS"
+
+RUN_ARGS="--rm"
+if [[ -n "$CIRCLECI"  ]] ; then
+  RUN_ARGS=""
+fi
+
+# set a default build -> 0 for when it doesn't exist
+CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
+
+# location to mount the source in the container
+inner_mount="/src"
+
+# epoch to use for -revision
+epoch=$(date +%s)
+
+case $CIRCLE_BRANCH in
+"master")
+  CHANNEL="release"
+  gittag
+  ;;
+"stage")
+  CHANNEL="stage"
+  ;;
+"yolo")
+  CHANNEL="yolo"
+  ;;
+*)
+  CHANNEL="dev"
+  ;;
+esac
+
+for ver in $BUILD_VERSIONS; do
+    echo "==> Building wp-cli rpm for fedora $ver "
+
+    build_image=quay.io/getpantheon/rpmbuild-fedora:${ver}
+    $docker pull $build_image
+
+    exec_cmd="$inner_mount/scripts/docker-inner.sh $CHANNEL $inner_mount/pkg $CIRCLE_BUILD_NUM $epoch"
+    if [ -n "$BUILD_DEBUG" ] ; then
+      RUN_ARGS="$RUN_ARGS -ti "
+      exec_cmd="/bin/bash"
+    fi
+
+    read docker_cmd <<-EOL
+      $docker run $RUN_ARGS \
+        -e "build=$CIRCLE_BUILD_NUM" \
+        -w $inner_mount \
+        -v $bin/../:$inner_mount \
+        $build_image $exec_cmd
+EOL
+
+
+    echo "Running: $docker_cmd"
+    $docker_cmd
+done
